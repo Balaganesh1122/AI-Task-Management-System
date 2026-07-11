4175458fe8d333a13ca13ab61be591040bea126d
Balaganesh <ganeshgolla2019@gmail.com>
2026-07-11 15:27:42 +0530

    Completed authentication module with JWT login

--- backend/app/api/auth.py
+++ backend/app/api/auth.py
@@ -4,6 +4,9 @@ from sqlalchemy.orm import Session
 from app.database.connection import get_db
 from app.schemas.auth import RegisterRequest
 from app.services.auth_service import create_user
+from app.schemas.auth import LoginRequest
+from app.services.auth_service import login_user
+from fastapi import HTTPException
 
 router = APIRouter(
     prefix="/api/auth",
@@ -19,3 +22,16 @@ def register(user: RegisterRequest, db: Session = Depends(get_db)):
         "message": "User registered successfully",
         "user_id": str(new_user.id)
     }
+
+@router.post("/login")
+def login(user: LoginRequest, db: Session = Depends(get_db)):
+
+    token = login_user(db, user.email, user.password)
+
+    if not token:
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid email or password"
+        )
+
+    return token
--- backend/app/services/auth_service.py
+++ backend/app/services/auth_service.py
@@ -1,10 +1,21 @@
 from sqlalchemy.orm import Session
+from fastapi import HTTPException
 
 from app.models.user import User
-from app.core.security import hash_password
+from app.core.security import hash_password, verify_password
+from app.utils.jwt_handler import create_access_token
 
 
 def create_user(db: Session, user):
+    # Check if email already exists
+    existing_user = db.query(User).filter(User.email == user.email).first()
+
+    if existing_user:
+        raise HTTPException(
+            status_code=409,
+            detail="Email already registered"
+        )
+
     hashed_password = hash_password(user.password)
 
     db_user = User(
@@ -22,3 +33,28 @@ def create_user(db: Session, user):
     db.refresh(db_user)
 
     return db_user
+
+
+def login_user(db: Session, email: str, password: str):
+    # Find user by email
+    user = db.query(User).filter(User.email == email).first()
+
+    if not user:
+        return None
+
+    # Verify password
+    if not verify_password(password, user.password):
+        return None
+
+    # Generate JWT token
+    token = create_access_token(
+        {
+            "sub": user.email,
+            "role": user.role
+        }
+    )
+
+    return {
+        "access_token": token,
+        "token_type": "bearer"
+    }
